cac735416bb7ffd08daee9fb8dc28ee28a59a0f8
CKalnasy <kalnasy.6@osu.edu>
2016-01-29 12:49:50 -0500

    0.2.6

--- SwiftSerialize/Init.sh
+++ SwiftSerialize/Init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 command -v php >/dev/null 2>&1 || { echo >&2 "PHP is requried, but is not installed.  Aborting."; exit 1; }
-chmod 666 InitializerExtension.swift
-php Scripts/Init.php $1
+DIR="`dirname \"$0\"`"
+chmod 666 $DIR/InitializerExtension.swift
+php $DIR/Scripts/Init.php $1
